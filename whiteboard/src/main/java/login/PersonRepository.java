e00afe104704c7f75fb786b9e7986f652cd74b53
Yealim <ysung@iastate.edu>
2018-02-19 09:27:46 -0600

    commented out the part that is not working properly for demo

--- whiteboard/src/main/java/login/PersonRepository.java
+++ whiteboard/src/main/java/login/PersonRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
 public interface PersonRepository extends Repository<Person, Long> {
-	@Query("SELECT person FROM Person person left join fetch person.username WHERE person.id =:id")
+//	@Query("SELECT person FROM Person person left join fetch person.username WHERE person.id =:id")
 	Person findById(@Param("id")int id);
 	void save(Person p);
 	
