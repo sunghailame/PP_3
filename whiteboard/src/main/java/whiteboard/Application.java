1b8a229adcbc691065e4fe4dc9b6bc8774d265bc
sacoffey <sacoffey@iastate.edu>
2018-02-21 10:54:38 -0600

    Jar buildable version!

--- whiteboard/src/main/java/whiteboard/Application.java
+++ whiteboard/src/main/java/whiteboard/Application.java
@@ -2,9 +2,6 @@ package whiteboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import groovy.lang.Grab;
-
-//@Grab("org.webjars:jquery:2.0.3-1:")
 
 @SpringBootApplication
 public class Application {
