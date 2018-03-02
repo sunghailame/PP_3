9831d73ef7cc80a5f25507f96d582db2a3a682fa
sacoffey <sacoffey@iastate.edu>
2018-03-02 14:39:53 -0600

    Added view user list functionality with dummy data to admin

--- whiteboard/src/main/java/whiteboard/admin/AdminController.java
+++ whiteboard/src/main/java/whiteboard/admin/AdminController.java
@@ -3,6 +3,8 @@ package whiteboard.admin;
 import whiteboard.login.Person;
 import whiteboard.login.PersonRepository;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -10,8 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+
 @Controller
 public class AdminController {
+	
+	
+	//Just a dummy list for testing purposes. Delete when repo added.
+	public ArrayList<Person> dummyList(){
+		
+		ArrayList<Person> people = new ArrayList<Person>();
+		
+		for(int i=0;i< 10;i++) {
+			people.add(new Person("username"+i, "password"+i, "name"+i, "email"+i, "role"+i, i));
+		}
+		
+		return people;
+	}
+	
+	
     @GetMapping("/admin/admin_home")
     public String signup_from_login(@CookieValue("person") String person, Model model) {
 	  	System.out.println(person);
@@ -33,4 +52,14 @@ public class AdminController {
     public String login_from_signup(@ModelAttribute Person person) {
         return "login/greeting";
     }
+     
+     @GetMapping("/admin/show_users")
+     public String show_users_from_admin(Person person, Model model) {
+    	 //TODO: Replace with repo findAll call
+     	ArrayList<Person> allPeople = dummyList();
+     	model.addAttribute("users", allPeople);
+     	
+         return "admin/show_users";
+     }
 }
+
