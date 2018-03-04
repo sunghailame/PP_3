02291a501a5b68442908200b39473abd045d9595
SydneyTeh <steh@iastate.edu>
2018-03-03 18:23:42 -0600

    Added Enrollment and Courses"

--- whiteboard/src/main/java/whiteboard/admin/Admin.java
+++ whiteboard/src/main/java/whiteboard/admin/Admin.java
@@ -1,19 +1,19 @@
-//package whiteboard.admin;
-//
-//import whiteboard.login.Person;
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.JoinColumn;
-//
-//@Entity
-//@Table(name = "Admin")
-//public class Admin {
-//
-//	@ManyToOne
-//	@JoinColumn(name = "ID")
-//	private Person person;
-//	
-//	
-//	
-//}
+package whiteboard.admin;
+
+import whiteboard.login.Person;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
+
+@Entity
+@Table(name = "Admin")
+public class Admin {
+	
+	@ManyToOne
+	@JoinColumn(name = "ID")
+	private Person person;	
+}
+
+
