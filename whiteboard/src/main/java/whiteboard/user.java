a959b68d4a5531e92b50d5281f593f668215de6a
mi1 <mi1@iastate.edu>
2018-03-19 23:48:42 -0500

    admin home

---
+++ whiteboard/src/main/java/whiteboard/user.java
@@ -0,0 +1,37 @@
+package whiteboard;
+
+import javax.persistence.Entity;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+@Entity
+public class user {
+	
+	@NotEmpty
+	public String username;
+	public String password;
+	
+	public user() {
+
+	}
+	
+	public user(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
