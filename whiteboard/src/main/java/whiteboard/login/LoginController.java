169405df6f5e0d3b7385d3f5d7124a59a6e90da0
sacoffey <sacoffey@iastate.edu>
2018-03-21 13:35:04 -0500

    Changed PersonController to LoginController

--- whiteboard/src/main/java/whiteboard/login/LoginController.java
+++ whiteboard/src/main/java/whiteboard/login/LoginController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class PersonController {
+public class LoginController {
 	@Autowired
 
 	private PersonRepository PersonRepository;
