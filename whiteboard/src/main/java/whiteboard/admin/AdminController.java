b02aaf0e1fffda37f419805438cf5740c79b9cbc
sacoffey <sacoffey@iastate.edu>
2018-03-22 20:19:57 -0500

    Added enroll prof to controller

--- whiteboard/src/main/java/whiteboard/admin/AdminController.java
+++ whiteboard/src/main/java/whiteboard/admin/AdminController.java
@@ -89,8 +89,10 @@ public class AdminController {
 		Iterator<Person> u_cur = users_temp.iterator();
 		while(u_cur.hasNext()) {
 			Person user = (Person) u_cur.next();
-			EnrollPerson p = new EnrollPerson(user.id, false, user.username);
-			users.add(p);
+			if(user.role.toUpperCase().contains("STUDENT")) {
+				EnrollPerson p = new EnrollPerson(user.id, false, user.username);
+				users.add(p);
+			}
 		}
 		
 		//Get list of all courses, save to list of EnrollCourse type for form
@@ -117,6 +119,66 @@ public class AdminController {
 	//TODO: Add checking for if a user is already enrolled in a course - un-enroll them?
 	@PostMapping("/admin/enroll_student")
 	public String admin_home_from_enroll_student(@RequestParam("enrolled") List<String> enroll_users, @RequestParam("c_enrolled") String enroll_course, Model model) {
+		String courseCode;
+		try {
+			//Parse the string data send back from the view
+			//One course and students to enroll
+			String[] splitCourse = enroll_course.split("=");
+			courseCode = splitCourse[0];
+			
+			Iterator<String> u_cur = enroll_users.iterator();
+			while(u_cur.hasNext()) {
+				
+				String[] splitUser = u_cur.next().split("=");
+				this.enrollmentRepository.save(new Enrollment(Integer.parseInt(splitUser[0]), courseCode, "1"));
+			}
+			
+			model.addAttribute("message", "Enrolled Students!");
+		} catch (Exception E) {
+			model.addAttribute("message", "Error enrolling. Try again.");
+		}
+		return "admin/admin_home";
+	}
+	
+	@GetMapping("/admin/enroll_prof")
+	public String enroll_prof_from_admin(Model model) {
+		
+		//Get list of all students, save to list of EnrollPerson type for form
+		Iterable<Person> users_temp = adminRepository.findAll();
+		ArrayList<EnrollPerson> users = new ArrayList<>();
+		Iterator<Person> u_cur = users_temp.iterator();
+		while(u_cur.hasNext()) {
+			Person user = (Person) u_cur.next();
+			if(user.role.toUpperCase().contains("PROF")) {
+				EnrollPerson p = new EnrollPerson(user.id, false, user.username);
+				users.add(p);
+			}
+		}
+		
+		//Get list of all courses, save to list of EnrollCourse type for form
+		Iterable<Course> course_temp = courseRepository.findAll();
+		ArrayList<EnrollCourse> courses = new ArrayList<>();
+		Iterator<Course> c_cur = course_temp.iterator();
+		while(c_cur.hasNext()) {
+			Course course = c_cur.next();
+			EnrollCourse m = new EnrollCourse(course.course_code, course.course_name, false);
+			courses.add(m);
+		}
+		
+		//Create wrapper for sending to view with users and courses
+		FormWrapper userList = new FormWrapper();
+		userList.setUsers(users);
+		userList.setCourses(courses);
+		
+		//Add the list to the view
+		model.addAttribute("userList", userList);
+		model.addAttribute("message","");
+		return "admin/enroll_student";
+	}
+	
+	//TODO: Add checking for if a user is already enrolled in a course - un-enroll them?
+	@PostMapping("/admin/enroll_prof")
+	public String admin_home_from_enroll_prof(@RequestParam("enrolled") List<String> enroll_users, @RequestParam("c_enrolled") String enroll_course, Model model) {
 		String courseCode;
 		try {
 			//Parse the string data send back from the view
