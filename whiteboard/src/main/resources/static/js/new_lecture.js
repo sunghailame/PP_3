6a9ae4b4f297282124f51e5a5a9b670a40caa29f
mi1 <mi1@iastate.edu>
2018-03-25 18:02:15 -0500

    new lecture functionalities

---
+++ whiteboard/src/main/resources/static/js/new_lecture.js
@@ -0,0 +1,24 @@
+new_lecture();
+
+
+function new_lecture(){
+
+var title = document.getElementById("title");
+var details = document.getElementById("details");
+var link = document.getElementById("link");
+var submit = document.getElementById("submit");
+ 
+submit.addEventListener("click", function(){
+
+
+if(title.value.length == 0 || details.value.length ==0 || link.value.length){
+
+	alert("A Field is empty");
+	title.value = "";
+	
+}
+
+
+
+});
+}
