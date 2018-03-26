eac042e40108132d34145c0ebe5ac34124ba4473
mi1 <mi1@iastate.edu>
2018-03-25 19:17:17 -0500

    pages

--- whiteboard/src/main/resources/static/js/new_lecture.js
+++ whiteboard/src/main/resources/static/js/new_lecture.js
@@ -11,7 +11,7 @@ var submit = document.getElementById("submit");
 submit.addEventListener("click", function(){
 
 
-if(title.value.length == 0 || details.value.length ==0 || link.value.length){
+if(title.value.length == 0 || details.value.length ==0 || link.value.length==0){
 
 	alert("A Field is empty");
 	title.value = "";
