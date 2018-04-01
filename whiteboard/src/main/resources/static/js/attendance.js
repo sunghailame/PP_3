e27e7eec18c2995347f9b3dd31e1ede7ffd2e3ab
mi1 <mi1@iastate.edu>
2018-04-01 14:44:20 -0500

    attendance handling

---
+++ whiteboard/src/main/resources/static/js/attendance.js
@@ -0,0 +1,19 @@
+takeAttendance();
+
+function takeAttendance(){
+
+var attendanceBox = document.getElementById("attendance");
+
+
+
+if(attendanceBox.checked == true){
+
+	alert("checked!");
+
+	
+}
+
+
+
+});
+}
