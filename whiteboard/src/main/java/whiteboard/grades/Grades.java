99c0411f50b49b774a3740fc877476cf2da724a9
Yealim <ysung@iastate.edu>
2018-04-04 16:25:19 -0500

    skeleton grades

--- whiteboard/src/main/java/whiteboard/grades/Grades.java
+++ whiteboard/src/main/java/whiteboard/grades/Grades.java
@@ -1,12 +1,65 @@
 package whiteboard.grades;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 //Commented because of entityManager exceptions
-//@Entity
-//@Table(name="Grades")
+@Entity
+@Table(name="Grades")
 public class Grades {
 
+	@Column(name = "CourseCode")
+	public String courseCode;
+	@Column(name = "Assignments")
+	public int assignments;
+	@Column(name = "Attendance")
+	public int attendance;
+	@Column(name = "Exams")
+	public int exams;
 	
+	public Grades(String courseCode, int assignments, int attendance, int exams) {
+		super();
+		this.courseCode = courseCode;
+		this.assignments = assignments;
+		this.attendance = attendance;
+		this.exams = exams;
+	}
+	
+	public Grades() {
+		
+	}
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	
+	public void setAssignments(int assignments) {
+		this.assignments = assignments;
+	}
+	
+	public int getAssignments() {
+		return assignments;
+	}
+	
+	public void setAttendance(int attendance) {
+		this.attendance = attendance;
+	}
+	
+	public int getAttendance() {
+		return attendance;
+	}
+	
+	public void setExams(int exams) {
+		this.exams = exams;
+	}
+	
+	public int getExams() {
+		return exams;
+	}
+	
+	@Override
+	public String toString() {
+		return this.courseCode+"===="+this.assignments+"===="+this.attendance+"===="+this.exams;
+	}
 }
