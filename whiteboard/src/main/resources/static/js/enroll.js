ffb8e3a8d7503aa501dd83d711e64f7917e1e9ae
mi1 <mi1@iastate.edu>
2018-04-23 10:58:54 -0500

    admin checks

---
+++ whiteboard/src/main/resources/static/js/enroll.js
@@ -0,0 +1,26 @@
+enroll();
+
+
+function enroll() {
+
+	
+		
+		document.getElementById("sub").addEventListener("click", function(){
+		
+			if(document.getElementById("courseSelected").selected != true){
+		        alert("Please select course");
+			}
+			if(document.getElementById("studentChecked").selected != true){	
+			   	alert("Please select atleast one student");
+			}	
+			if(document.getElementById("courseRadio").selected != true){
+				alert("Please select a course");    
+			}	         
+			if(document.getElementById("ProfCheckBox").selected != true){
+				alert("Please select a Professor");    
+			}	 
+				      			      
+		
+		});
+		
+}
